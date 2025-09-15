bbc50d8084828647d0b32453fbba99a6aa1edc86
progamacaorute <rutefbertoldi@outlook.com>
2025-09-15 20:54:42 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(){
-printf("Desafio Super Trunfo\n");
+  char cidade[20] = Fortaleza;
+  int populaçãoFortaleza = 994464;
+  double areaFortaleza = 312.441;  //km²
+  double PIBFortaleza = 73400000000.0; // 73,4 bilhões em reais
+  float pontosturisticosFortaleza = 12;
 
+  printf
 
 }
