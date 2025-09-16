3bccce2d4f4c86340ea641f4df0bd862c8b110dc
progamacaorute <rutefbertoldi@outlook.com>
2025-09-15 21:07:30 -0300

    Update Fortaleza CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,10 @@ int main(){
   double PIBFortaleza = 73400000000.0; // 73,4 bilhões em reais
   float pontosturisticosFortaleza = 12;
 
-  printf
+  printf("Cidade: %s\n", cidade);
+  printf("População de Fortaleza: %d habitante\n", populaçãoFortaleza);
+  printf("Área de Fortaleza: %.3f km²\n", areaFortaleza);
+  printf("PIB de Fortaleza: %.2f reais\n", PIBFortaleza);
+  printf("Pontos Turísticos: %f\n", pontosturisticosFortaleza);
 
 }
